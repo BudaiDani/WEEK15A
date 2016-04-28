02402a1ceb6b8fb74092dd2b2e5027b63aa57cbe
Budai Dani <budai.d945@gmail.com>
2016-04-28 10:35:48 +0200

    ex 2 done in serializable

--- Serialization/CreateClass/CreateClass/Program.cs
+++ Serialization/CreateClass/CreateClass/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace CreateClass
 {
@@ -10,14 +13,24 @@ namespace CreateClass
     {
         static void Main(string[] args)
         {
-            Employee Kovacs = new Employee("Géza", DateTime.Now, 1000, "léhűtő");
-            Kovacs.Room = new Room(111);
-            Employee Kovacs2 = (Employee)Kovacs.Clone();
+            Employee Budai = new Employee("Dani", DateTime.Now, 1000, "HS");
+            Budai.Room = new Room(111);
+            Employee Kovacs2 = (Employee)Budai.Clone();
             Kovacs2.Room.Number = 112;
 
-            Console.WriteLine(Kovacs.ToString());
+            Console.WriteLine(Budai.ToString());
             Console.WriteLine(Kovacs2.ToString());
             Console.ReadKey();
+        }
+
+        private static void Serialize(Person sp)
+        {
+            FileStream fs = new FileStream("Person.Dat", FileMode.Create);
+            BinaryFormatter bf = new BinaryFormatter();
+
+            bf.Serialize(fs, sp);
+
+            fs.Close();
         }
     }
 }
