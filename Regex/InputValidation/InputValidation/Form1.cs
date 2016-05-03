054b7a1653771868f75e5e969133aed097b0240e
Budai Dani <budai.d945@gmail.com>
2016-05-03 11:46:57 +0200

    email corrected

--- Regex/InputValidation/InputValidation/Form1.cs
+++ Regex/InputValidation/InputValidation/Form1.cs
@@ -28,9 +28,10 @@ namespace InputValidation
 
             txtPhone.Text = ReformatPhone(txtPhone.Text);
 
-            if (!Regex.IsMatch(txtEmail.Text, 
-                @"^([a-zA-Z0-9_\-” [email protected]\.]+)@((\[[0-9]{1,3}" + 
-                @"\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\" + 
+            if (!Regex.IsMatch(txtEmail.Text,
+                @"^([a-zA-Z0-9_\-” +
+                @”\.]+)@((\[[0-9]{1,3}" +
+                @"\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\" +
                 @".)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$"))
                 MessageBox.Show("The e-mail address is not valid.");
 
